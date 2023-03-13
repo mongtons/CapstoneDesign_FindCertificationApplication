e777f31403a76e00ae649f1ea0f193a670629241
mongtons <mongtons990213@gmail.com>
2023-03-13 17:12:46 +0900

    Edit BoardActivity.kt and activity_board.xml

--- app/src/main/java/hallym/capstone/findcertificateapplication/BoardActivity.kt
+++ app/src/main/java/hallym/capstone/findcertificateapplication/BoardActivity.kt
@@ -2,10 +2,26 @@ package hallym.capstone.findcertificateapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import hallym.capstone.findcertificateapplication.databinding.ActivityBoardBinding
 
 class BoardActivity : AppCompatActivity() {
+    val binding by lazy {
+        ActivityBoardBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_board)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        binding.title.text=intent.getStringExtra("title")
+        binding.user.text=intent.getStringExtra("user")
+        binding.time.text=intent.getStringExtra("time")
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return super.onSupportNavigateUp()
     }
 }
