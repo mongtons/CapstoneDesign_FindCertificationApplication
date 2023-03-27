d3abfd04754234ab4101d0ac2d5aeb291f2740bf
cclo <ccloseon@gmail.com>
2023-03-27 18:40:34 +0900

    add a

--- app/src/main/java/hallym/capstone/findcertificateapplication/MainActivity.kt
+++ app/src/main/java/hallym/capstone/findcertificateapplication/MainActivity.kt
@@ -13,6 +13,8 @@ class MainActivity : AppCompatActivity() {
     }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val a = 1
         setContentView(binding.root)
 
         setSupportActionBar(binding.toolbar)
