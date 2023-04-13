04a4ee8d92ed184cb8a475560226d5750d97c096
mongtons <mongtons990213@gmail.com>
2023-04-13 12:17:06 +0900

    Connect to Open AI API, and Do chating

--- app/src/main/java/hallym/capstone/findcertificateapplication/mainfragment/AiFragment.kt
+++ app/src/main/java/hallym/capstone/findcertificateapplication/mainfragment/AiFragment.kt
@@ -1,20 +1,117 @@
 package hallym.capstone.findcertificateapplication.mainfragment
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.EditorInfo
+import android.widget.TextView
+import android.widget.Toast
+import com.android.volley.RequestQueue
+import com.android.volley.Response
+import com.android.volley.RetryPolicy
+import com.android.volley.VolleyError
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
+import com.google.android.material.textfield.TextInputEditText
 import hallym.capstone.findcertificateapplication.R
+import hallym.capstone.findcertificateapplication.databinding.FragmentAiBinding
+import org.json.JSONObject
 
 class AiFragment : Fragment() {
+    lateinit var responseTV: TextView
+    lateinit var questionTV: TextView
+    lateinit var queryEdt: TextInputEditText
+
+    var url = "https://api.openai.com/v1/completions"
+    val apiKey="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"
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        val gpt = chatGPT()
-        gpt.chatRun()
-        return inflater.inflate(R.layout.fragment_ai, container, false)
+        super.onCreate(savedInstanceState)
+        val binding= FragmentAiBinding.inflate(inflater, container, false)
+
+        // initializing variables on below line.
+        responseTV = binding.idTVResponse
+        questionTV = binding.idTVQuestion
+        queryEdt = binding.idEdtQuery
+        // adding editor action listener for edit text on below line.
+        queryEdt.setOnEditorActionListener(TextView.OnEditorActionListener { v, actionId, event ->
+            if (actionId == EditorInfo.IME_ACTION_SEND) {
+                // setting response tv on below line.
+                responseTV.text = "Please wait.."
+                // validating text
+                if (queryEdt.text.toString().length > 0) {
+                    // calling get response to get the response.
+                    getResponse(queryEdt.text.toString())
+                } else {
+                    Toast.makeText(context, "Please enter your query..", Toast.LENGTH_SHORT).show()
+                }
+                return@OnEditorActionListener true
+            }
+            false
+        })
+        return binding.root
+    }
+    private fun getResponse(query: String) {
+        // setting text on for question on below line.
+        questionTV.text = query
+        queryEdt.setText("")
+        // creating a queue for request queue.
+        val queue: RequestQueue = Volley.newRequestQueue(context)
+        // creating a json object on below line.
+        val jsonObject: JSONObject = JSONObject()
+        // adding params to json object.
+        jsonObject.put("model", "text-davinci-003")
+        jsonObject.put("prompt", query)
+        jsonObject.put("temperature", 0)
+        jsonObject.put("max_tokens", 100)
+        jsonObject.put("top_p", 1)
+        jsonObject.put("frequency_penalty", 0.0)
+        jsonObject.put("presence_penalty", 0.0)
+
+        // on below line making json object request.
+        val postRequest: JsonObjectRequest =
+            // on below line making json object request.
+            object : JsonObjectRequest(Method.POST, url, jsonObject,
+                Response.Listener { response ->
+                    // on below line getting response message and setting it to text view.
+                    val responseMsg: String =
+                        response.getJSONArray("choices").getJSONObject(0).getString("text")
+                    responseTV.text = responseMsg
+                },
+                // adding on error listener
+                Response.ErrorListener { error ->
+                    Log.e("TAGAPI", "Error is : " + error.message + "\n" + error)
+                }) {
+                override fun getHeaders(): MutableMap<String, String> {
+                    val params: MutableMap<String, String> = HashMap()
+                    // adding headers on below line.
+                    params["Content-Type"] = "application/json"
+                    params["Authorization"] = "Bearer $apiKey"
+                    return params
+                }
+            }
+
+        // on below line adding retry policy for our request.
+        postRequest.setRetryPolicy(object : RetryPolicy {
+            override fun getCurrentTimeout(): Int {
+                return 50000
+            }
+
+            override fun getCurrentRetryCount(): Int {
+                return 50000
+            }
+
+            @Throws(VolleyError::class)
+            override fun retry(error: VolleyError) {
+            }
+        })
+        // on below line adding our request to queue.
+        queue.add(postRequest)
     }
 }
