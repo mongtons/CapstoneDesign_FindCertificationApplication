88e3fe2d68801b83ac6f00211a6bc3626c7764ef
minwo <minwoo9676@gmail.com>
2023-04-28 16:20:21 +0900

    commit

--- app/src/main/java/hallym/capstone/findcertificateapplication/CalendarActivity.kt
+++ app/src/main/java/hallym/capstone/findcertificateapplication/CalendarActivity.kt
@@ -2,19 +2,32 @@ package hallym.capstone.findcertificateapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.PagerSnapHelper
+import hallym.capstone.findcertificateapplication.calendar.AdapterMonth
 import hallym.capstone.findcertificateapplication.databinding.ActivityCalendarBinding
 
+
+
 class CalendarActivity : AppCompatActivity() {
-    val binding by lazy {
+    val binding by lazy{
         ActivityCalendarBinding.inflate(layoutInflater)
     }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-    }
-    override fun onSupportNavigateUp(): Boolean {
-        onBackPressed()
-        return super.onSupportNavigateUp()
+        val monthListManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+        val monthListAdapter = AdapterMonth()
+
+        binding.calendarCustom.layoutManager = monthListManager
+        binding.calendarCustom.adapter = monthListAdapter
+//        binding.calendarCustom.addItemDecoration(
+//        )
+
+            //scrollToPosition(Int.MAX_VALUE/2)
+
+        val snap = PagerSnapHelper()
+        snap.attachToRecyclerView(binding.calendarCustom)
     }
 }
---
+++ app/src/main/java/hallym/capstone/findcertificateapplication/calendar/AdapterDay.kt
@@ -0,0 +1,64 @@
+package hallym.capstone.findcertificateapplication.calendar
+
+import android.graphics.Color
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
+import hallym.capstone.findcertificateapplication.R
+import hallym.capstone.findcertificateapplication.databinding.ActivityCalendarBinding
+import hallym.capstone.findcertificateapplication.databinding.ActivityCommunityBinding
+import hallym.capstone.findcertificateapplication.databinding.ListItemDayBinding
+import kotlinx.coroutines.NonDisposableHandle.parent
+import java.util.*
+
+class ViewHolder(val binding : ListItemDayBinding):RecyclerView.ViewHolder(binding.root)
+
+class AdapterDay(val tempMonth:Int, val dayList: MutableList<Date>): RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+    val ROW = 6
+
+    //inner class DayView(val layout: View): RecyclerView.ViewHolder(layout)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder  =
+        ViewHolder(ListItemDayBinding.inflate(LayoutInflater.from(parent.context),
+        parent, false))
+//        var view = LayoutInflater.from(parent.context).inflate(R.layout.list_item_day, parent, false)
+//        return DayView(view)
+
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        val binding = (holder as ViewHolder).binding
+        binding.itemDayLayout.setOnClickListener {
+            Toast.makeText( binding.itemDayLayout.context, "${dayList[position]}", Toast.LENGTH_SHORT).show()
+        }
+//        holder.layout.item_day_layout.setOnClickListener {
+//            Toast.makeText(holder.layout.context, "${dayList[position]}", Toast.LENGTH_SHORT).show()
+//        }
+        binding.itemDayText.text = dayList[position].date.toString()
+        //holder.layout.item_day_text.text = dayList[position].date.toString()
+
+        binding.itemDayText.setTextColor(when(position % 7) {
+            0 -> Color.RED
+            6 -> Color.BLUE
+            else -> Color.BLACK
+        })
+//        holder.layout.item_day_text.setTextColor(when(position % 7) {
+//            0 -> Color.RED
+//            6 -> Color.BLUE
+//            else -> Color.BLACK
+//        })
+
+        if(tempMonth != dayList[position].month) {
+            binding.itemDayText.alpha = 0.4f
+//            holder.layout.item_day_text.alpha = 0.4f
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return ROW * 7
+    }
+}
